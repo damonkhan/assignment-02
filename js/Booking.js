73e497d1dd0302872aba97db1574e4b808de7129
damon <damokhan92@gmail.com>
2018-08-08 13:44:09 +1200

    Adding price logic

--- js/Booking.js
+++ js/Booking.js
@@ -1,6 +1,19 @@
+const SINGLE_BASE_PRICE = 55;
+const DOUBLE_BASE_PRICE = 100;
+const STUDIO_BASE_PRICE = 125;
+const ADULT_PRICE = 50;
+const CHILD_PRICE = 25;
+const BREAKFAST_PRICE = 30;
+
 let checkIn = document.querySelector("#check-in");
 let checkOut = document.querySelector("#check-out");
 let arrivalInput = document.querySelector("#arrival");
+let roomType = document.querySelector("#room-type");
+let rooms = document.querySelector("#num-rooms");
+let adults = document.querySelector("#num-adults");
+let child = document.querySelector("#num-children");
+let breakfast = document.querySelector("#checkbox-breakfast");
+let reset = document.querySelector("#reset");
 
 /*
 *displays arrival time dropdown once check-in and check-out dates set
@@ -19,7 +32,7 @@ function checkDates() {
 * Checks that the check out date comes after the
 * check in date
  */
-function getNumDays() {
+function checkDatePicker() {
     // The numeric representation of the check in day.
     let checkinDay = Number(checkIn.value.substring(checkIn.value.length - 2, checkIn.value.length));
     let checkoutDay = Number(checkOut.value.substring(checkOut.value.length - 2, checkOut.value.length));
@@ -68,11 +81,64 @@ function setCheckOutDate() {
     checkOut.value = date;
 }
 
+/*
+* Gets the num nights a guest is staying
+* @ return: num nights (Number)
+ */
+function getNumNights() {
+
+    return Number(checkOut.value.substring(checkOut.value.length - 2, checkOut.value.length)) - Number(checkIn.value.substring(checkIn.value.length - 2, checkIn.value.length));
+}
+
+/*
+* Calculates the total cost based on:
+* Unit type, num nights, num rooms, num adults, num children, breakfast (optional)
+* @textContent: Total cost
+ */
+function calculateCost() {
+    let unitType = document.querySelector("#room-type").value;
+    let numNights = getNumNights();
+    let numRooms = Number(document.querySelector("#num-rooms").value);
+    let numAdults = Number(document.querySelector("#num-adults").value);
+    let numChildren = Number(document.querySelector("#num-children").value);
+    let breakfast = document.querySelector("#checkbox-breakfast").checked;
+    let total = 0;
+
+    if (unitType !== "none" && numNights !== 0) {
+
+        if (unitType === "single") {
+            total = numNights * SINGLE_BASE_PRICE;
+        } else if (unitType === "double") {
+            total = numNights * DOUBLE_BASE_PRICE;
+        } else {
+            total = numNights * STUDIO_BASE_PRICE;
+        }
+
+        total = total + ((numAdults * ADULT_PRICE) + (numChildren * CHILD_PRICE));
+        total *= numRooms;
+
+        if (breakfast) {
+            var cost = (BREAKFAST_PRICE * numAdults) + (BREAKFAST_PRICE * numChildren);
+            total = cost + total;
+        }
+
+        document.querySelector("#total").textContent = total.toString();
+    }
+}
+
 
 // Unobtrusive JavaScript
 checkIn.addEventListener("change", setCheckOutDate);
 checkIn.addEventListener("change", checkDates);
+checkIn.addEventListener("change", calculateCost);
 checkOut.addEventListener("change", checkDates);
-checkOut.addEventListener("change", getNumDays);
+checkOut.addEventListener("change", checkDatePicker);
+checkOut.addEventListener("change", calculateCost);
+roomType.addEventListener("change", calculateCost);
+adults.addEventListener("change", calculateCost);
+child.addEventListener("change", calculateCost);
+breakfast.addEventListener("change", calculateCost);
+rooms.addEventListener("change", calculateCost);
+reset.addEventListener("click", function () { document.querySelector("#total").textContent = "0";} );
 
 
